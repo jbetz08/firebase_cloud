2e71446d17e0e4e5dfad0bf6fe6ad581de49c0e5
Josh Betzsold <bet16011@byui.edu>
2021-09-22 13:38:24 -0600

    connect firebase and SA

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firebase 
+
+#use a service account
+cred = credentials.Certificate('scotts-totts-firebase-ce5beb7102d0.json')
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+
 print("Firebase") 
