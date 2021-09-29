1c18059fe78b27970ec430cdd9411a6d1ee21d30
Josh Betzsold <bet16011@byui.edu>
2021-09-29 13:20:09 -0600

    deleting unneeded comments

--- main.py
+++ main.py
@@ -8,29 +8,7 @@ firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 #creating a new collection and a document
-'''
-doc_ref = db.collection(u'users').document(u'alovelace')
-doc_ref.set({
-    u'first': u'Ada',
-    u'last': u'Lovelace',
-    u'born': 1815
-})
-#creating another document to the 'users' collection
-doc_ref = db.collection(u'users').document(u'aturing')
-doc_ref.set({
-    u'first': u'Alan',
-    u'middle': u'Mathison',
-    u'last': u'Turing',
-    u'born': 1912
-})
-# verify that I added data to the cloud firestore
-users_ref = db.collection(u'users')
-docs = users_ref.stream()
 
-for doc in docs:
-    print(f'{doc.id} => {doc.to_dict()}')
-
-'''
 print("Firebase") 
 
 doc_ref = db.collection(u'test').document(u'bob')
